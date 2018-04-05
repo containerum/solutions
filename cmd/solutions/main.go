4d30f1e1bf6784cda23b2710b9f883611f4eaa01
Ilia Donchenko <iliad@me.com>
2018-04-05 17:18:39 +0300

    Updated info

--- cmd/solutions/main.go
+++ cmd/solutions/main.go
@@ -15,7 +15,6 @@ func main() {
 	app.Flags = flags
 	app.Action = initServer
 
-	fmt.Printf("Starting %s %s\n", app.Name, app.Version)
 	if err := app.Run(os.Args); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
