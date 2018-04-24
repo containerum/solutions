38d5ac57510de5061f2deb16e34995c0038908a4
Ilia Donchenko <iliad@me.com>
2018-04-24 12:02:12 +0300

    Move json-types models inside project

---
+++ pkg/models/user_solutions.go
@@ -0,0 +1,27 @@
+package models
+
+type UserSolutionsList struct {
+	Solutions []UserSolution `json:"solutions"`
+}
+
+type UserSolution struct {
+	Branch    string            `json:"branch"`
+	Env       map[string]string `json:"env"`
+	Template  string            `json:"template"`
+	Name      string            `json:"name"`
+	Namespace string            `json:"namespace"`
+}
+
+type DeploymentsList struct {
+	Deployments []*interface{} `json:"deployments"`
+}
+
+type ServicesList struct {
+	Services []*interface{} `json:"services"`
+}
+
+type RunSolutionResponce struct {
+	Created    int      `json:"created"`
+	NotCreated int      `json:"not_created"`
+	Errors     []string `json:"errors,omitempty"`
+}
